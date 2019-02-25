7eb23cc0109e54df4993110e4dc66430f4bdbf65
maocat <zk246422@163.com>
2019-02-25 17:35:46 +0800

    ...

---
+++ docx_w/docx_wolrd.py
@@ -0,0 +1,54 @@
+from docx import Document
+from docx.shared import Inches
+
+document = Document()
+
+document.add_heading('Document Title', 0)
+
+p = document.add_paragraph('A plain paragraph having some ')
+p.add_run('bold').bold = True
+p.add_run(' and some ')
+p.add_run('italic.').italic = True
+
+document.add_heading('Heading, level 1', level=1)
+document.add_paragraph('Intense quote', style='Intense Quote')
+
+document.add_paragraph(
+    'first item in unordered list', style='List Bullet'
+)
+document.add_paragraph(
+    'first item in ordered list', style='List Number'
+)
+import requests
+
+url = 'https://avatars2.githubusercontent.com/u/37769031?s=400&u=13d84a0f9ee9c9f2b7e08856be9cfaf51517a9b2&v=4'
+pic = requests.get(url)
+from io import BytesIO
+from PIL import Image
+
+string_io = BytesIO(pic.content)
+img = Image.open(string_io)
+im_bytes = BytesIO()
+img.save(im_bytes, format='JPEG')
+document.add_picture(im_bytes, width=Inches(1.25))
+
+records = (
+    (3, '101', 'Spam'),
+    (7, '422', 'Eggs'),
+    (4, '631', 'Spam, spam, eggs, and spam')
+)
+
+table = document.add_table(rows=1, cols=3)
+hdr_cells = table.rows[0].cells
+hdr_cells[0].text = 'Qty'
+hdr_cells[1].text = 'Id'
+hdr_cells[2].text = 'Desc'
+for qty, id, desc in records:
+    row_cells = table.add_row().cells
+    row_cells[0].text = str(qty)
+    row_cells[1].text = id
+    row_cells[2].text = desc
+
+document.add_page_break()
+
+document.save('demo.docx')
